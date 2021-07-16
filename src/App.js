80c58c5a6da2debaa152752b9ec470c708a496eb
sachinshukla636 <59618838+sachinshukla636@users.noreply.github.com>
2021-07-16 12:37:32 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from "react";
-import "./style.css";
+import React, { useState } from 'react';
+import './style.css';
 
 export default function App() {
+  const [data, setData] = useState(null);
+  const [print, setPrint] = useState(false);
+  function getData(val) {
+    console.log(val.target.value);
+    setData(val.target.value);
+  }
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1>Get Input box value</h1>
+      {/*<h2>{data}</h2>*/}
+      {print ? <h2>{data}</h2> : null}
+      <input type="text" onChange={getData} />
+      <button onClick={() => setPrint(true)}>Print data</button>
     </div>
   );
 }
